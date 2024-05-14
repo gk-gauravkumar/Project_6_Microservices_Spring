8121dc16fd2408e80fa9d33b2f5739d7f1bf0d7a
gk-gauravkumar <gaurav@Gauravs-Mac-mini.local>
2024-05-14 22:06:39 +0530

    created REST API for Ratings Services and tested.

---
+++ ratings-services/src/main/java/com/gk/ratingsservices/repository/RatingsRepository.java
@@ -0,0 +1,9 @@
+package com.gk.ratingsservices.repository;
+
+import com.gk.userservices.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,String> {
+}
