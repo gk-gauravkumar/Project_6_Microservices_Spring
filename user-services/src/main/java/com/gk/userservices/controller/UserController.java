a95e323ff9cdad90c4dedc46ff2fbdb403b00950
gk-gauravkumar <gaurav@Gauravs-Mac-mini.local>
2024-05-14 19:49:44 +0530

    SLF4J Logger implemented successfully

--- user-services/src/main/java/com/gk/userservices/controller/UserController.java
+++ user-services/src/main/java/com/gk/userservices/controller/UserController.java
@@ -2,6 +2,8 @@ package com.gk.userservices.controller;
 
 import com.gk.userservices.entity.User;
 import com.gk.userservices.services.UserServices;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
@@ -14,24 +16,28 @@ import java.util.List;
 @RequestMapping("/users")
 public class UserController {
 
+    private static Logger log = LoggerFactory.getLogger(UserController.class);
     @Autowired
     private UserServices userServices;
 
     @PostMapping
     public ResponseEntity<User> saveUser(@RequestBody User user){
         User user1 = userServices.saveUser(user);
+        log.info("User Details Saved Successfully");
         return ResponseEntity.ok(user1);
     }
 
     @GetMapping
     public ResponseEntity<List<User>> getAllUsers (){
         List<User> allUsersList = userServices.getAllUsers();
+        log.info("All Users Details Fetched Successfully");
         return ResponseEntity.ok(allUsersList);
     }
 
     @GetMapping("/{userId}")
     public ResponseEntity<User> getUserById(@PathVariable String userId){
         User user1 = userServices.getUserById(userId);
+        log.info("User Details Fetched Successfully");
         return ResponseEntity.ok(user1);
     }
 
