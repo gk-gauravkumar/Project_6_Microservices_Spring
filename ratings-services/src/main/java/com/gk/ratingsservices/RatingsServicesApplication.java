d98137a5e18251efb2078970a205b58111870475
gk-gauravkumar <gaurav@Gauravs-Mac-mini.local>
2024-05-16 11:27:34 +0530

    untracked file added

---
+++ ratings-services/src/main/java/com/gk/ratingsservices/RatingsServicesApplication.java
@@ -0,0 +1,13 @@
+package com.gk.ratingsservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatingsServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatingsServicesApplication.class, args);
+	}
+
+}
